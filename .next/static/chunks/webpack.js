6b8391d915128e77f085a659e01a52337feb26bf
Ali-Hassan333 <dev.hassan33@gmail.com>
2024-02-13 13:52:25 +0500

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "275f458a6ba7aab1"; }
+/******/ 		__webpack_require__.h = function() { return "a0e78d6f220c777b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
